17d3988f8d8e50b15aa4c5ffc3f0ba8233f2cad2
Elan Feingold <elan@plex.tv>
2021-03-19 13:26:03 -1000

    Don't ask a stopped server to stop.

--- ios/RCTHttpServer.m
+++ ios/RCTHttpServer.m
@@ -89,7 +89,8 @@ RCT_EXPORT_METHOD(stop)
     RCTLogInfo(@"Stopping HTTP bridge server");
     
     if (_webServer != nil) {
-        [_webServer stop];
+        if (_webServer.isRunning)
+          [_webServer stop];
         [_webServer removeAllHandlers];
         _webServer = nil;
     }
